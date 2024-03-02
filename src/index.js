fdec0b57b53b50daac0da30087573bf088b9cbc1
chris-0224 <chrisperez2589@gmail.com>
2024-03-01 22:18:22 -0500

    Added React Router for SPA with Home and Store pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Home from './home';
-import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+import './index.css';
+import Home from './routes/home';
+import reportWebVitals from './reportWebVitals';
+import ErrorPage from "./error-page";
+import Store from "./routes/store";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home/>,
+        errorElement: <ErrorPage/>
+    },
+    {
+        path: '/store',
+        element: <Store/>,
+        errorElement: <ErrorPage/>
+    }
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
