8d5252600445aa32b0c4a9bfd0b93489695ad7ff
chris-0224 <chrisperez2589@gmail.com>
2024-03-01 22:18:59 -0500

    Added media-promotion.js to act as a general promotion with image and text

---
+++ src/media-promotion.js
@@ -0,0 +1,18 @@
+import placeholder_img from "./placeholder-image.png";
+
+export default function MediaPromotion({title, img, desc, width, attribute}){
+    const image = img ? img : placeholder_img;
+
+    return (
+        <div className="media-promo-container">
+            <div className="image-container">
+                <img src={image} alt="promotion" width={width}/>
+                <cite>{attribute}</cite>
+            </div>
+            <div className="content-body-container">
+                <h2>{title}</h2>
+                <p>{desc}</p>
+            </div>
+        </div>
+    )
+}
