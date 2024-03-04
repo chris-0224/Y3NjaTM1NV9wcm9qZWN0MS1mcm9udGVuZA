4cf132f3f83bf4b9b6050ad5d104d8430ce7aa7e
chris-0224 <chrisperez2589@gmail.com>
2024-03-03 23:44:09 -0500

    Added categories-grid.js for the CategoriesGrid component to display relevant categories in a grid

---
+++ src/categories-grid.js
@@ -0,0 +1,31 @@
+import placeholder_img from "./placeholder-image.png";
+
+function CategoryItem({title, img}){
+    const image = img ? img : placeholder_img;
+
+    function clickEvent(e){
+        console.log("clicked!");
+    }
+
+    return (
+        <div className="category-item-container">
+            <button className="category-item-button" onClick={clickEvent}>
+                <img src={image} alt="promotion" width="200px" height="200px"/>
+            </button>
+            <h2 className="category-item-title">{title}</h2>
+        </div>
+    )
+}
+
+export default function CategoriesGrid({categoryNamesList}){
+    return (
+        <div className="categories-grid-container">
+            {categoryNamesList ?
+                categoryNamesList.map((catName)=>{
+                return <CategoryItem key={crypto.randomUUID()} title={catName}/>;
+            })
+                : <></>
+            }
+        </div>
+    )
+}
