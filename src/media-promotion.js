d73f2fc24e596928f08336656f5e6315821da0e8
chris-0224 <chrisperez2589@gmail.com>
2024-03-03 23:48:00 -0500

    Added ability to specify the id attribute

--- src/media-promotion.js
+++ src/media-promotion.js
@@ -1,10 +1,10 @@
 import placeholder_img from "./placeholder-image.png";
 
-export default function MediaPromotion({title, img, desc, width, attribute}){
+export default function MediaPromotion({title, img, desc, width, attribute, id}){
     const image = img ? img : placeholder_img;
 
     return (
-        <div className="media-promo-container">
+        <div className="media-promo-container" id={id}>
             <div className="image-container">
                 <img src={image} alt="promotion" width={width}/>
                 <cite>{attribute}</cite>
