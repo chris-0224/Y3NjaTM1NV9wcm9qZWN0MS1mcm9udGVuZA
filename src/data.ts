82b8cc318f3d86b0844eb52d49a25ec0027fc7b1
chris-0224 <chrisperez2589@gmail.com>
2024-03-08 00:02:00 -0500

    Added more mock data

--- src/data.ts
+++ src/data.ts
@@ -1,2 +1,44 @@
 
 export const dummyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper nisl a est iaculis porta. Ut tristique urna vel consequat pharetra. Proin egestas pharetra erat, sit amet mattis purus maximus nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut et ipsum tempus, tempus turpis porta, gravida urna. Ut ornare non nisl ut tempor. Phasellus ut massa facilisis, hendrerit enim et, elementum sem. Proin quis tortor non mi lacinia semper. Duis tincidunt lacinia dolor, et maximus tellus tristique ac. Nullam in orci vehicula, consequat tortor eget, porttitor turpis."
+export const dummyText2 = "Etiam consectetur consectetur tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean volutpat quis leo sed consectetur. Sed a lorem eu diam tempus condimentum. Quisque eros est, eleifend a porta eget, pellentesque vel dolor. Phasellus ullamcorper, massa eu cursus suscipit, mi nunc venenatis turpis, nec accumsan ex nibh ut augue. In id nisl luctus, fermentum turpis quis, scelerisque nisl. Pellentesque laoreet pretium bibendum. Praesent ac placerat diam. Cras consequat odio a quam scelerisque, ut elementum lacus tincidunt. Nullam et ipsum sit amet nunc elementum dictum.";
+export const dummyText3 = "Curabitur pulvinar lorem vitae orci cursus, ut consectetur arcu semper. Quisque sit amet orci id nisl interdum elementum aliquet ut velit. Sed a ex gravida, congue ligula vitae, iaculis leo. Aliquam placerat, lectus ac fringilla condimentum, mi neque ornare arcu, ac fringilla metus nisi sed justo. Fusce aliquam metus id faucibus cursus. Nunc egestas interdum metus, eu malesuada elit elementum sed. Donec mollis tincidunt ex, at aliquet justo pellentesque vel. Morbi consectetur aliquet massa nec porttitor. Vivamus tortor orci, rhoncus nec volutpat ut, porta sed diam. Cras quis rutrum dolor. Fusce rutrum tincidunt facilisis. Vivamus blandit ut arcu in semper. Cras sodales diam eu porttitor porttitor.";
+export const dummyText4 = "Sed porttitor metus efficitur, efficitur neque vitae, fermentum dui. Proin tempor sem eleifend nibh elementum volutpat. Nullam vel nibh eget velit interdum vestibulum in sit amet massa. Integer feugiat auctor congue. Proin iaculis nibh mauris, a congue nulla volutpat ut. Praesent sit amet imperdiet neque. Sed sed volutpat tortor. Suspendisse potenti. Cras eleifend urna posuere ipsum sagittis euismod.";
+export const dummyText5 = "Phasellus at quam eu lorem posuere sodales. Praesent pellentesque tempus tellus, vehicula bibendum tellus accumsan eu. Vivamus erat tortor, vulputate volutpat varius nec, gravida eget nulla. In a porttitor lacus. Cras ex diam, tempor in enim vitae, sodales aliquam arcu. Nam ligula nibh, posuere eget dictum eget, vestibulum non nibh. Praesent sollicitudin risus sed aliquam dignissim.";
+
+export const eventInformation = [
+    {
+        dateTime: "March 25, 2024 @ 12:45PM EST",
+        eventName: "Nam est nunc, suscipit eu eros nec",
+        description: "Pellentesque placerat magna eu sapien scelerisque tincidunt. Cras vel suscipit ante, non aliquet lacus.",
+        entranceFee: 5.00,
+        eventID: 1
+    },
+    {
+        dateTime: "April 12, 2024 @ 3:00PM EST",
+        eventName: "Proin at ullamcorper lacus",
+        description: "Quisque tincidunt convallis dui, ac sollicitudin nisl auctor eget.",
+        entranceFee: 10.00,
+        eventID: 2
+    },
+    {
+        dateTime: "April 29, 2024 @ 3:00PM EST",
+        eventName: "Morbi gravida sem pellentesque finibus rhoncus",
+        description: "Sed porttitor tortor ac velit volutpat tempus vel id erat. Sed risus lorem, aliquam id lectus a, ornare convallis felis.",
+        entranceFee: 5.00,
+        eventID: 3
+    },
+    {
+        dateTime: "May 20, 2024 @ 5:00PM EST",
+        eventName: "Maecenas lobortis lorem vel lectus maximus commodo",
+        description: "Ut vitae commodo augue, vitae tincidunt sapien. Maecenas commodo pretium nisl eget aliquet.",
+        entranceFee: 11.00,
+        eventID: 4
+    },
+    {
+        dateTime: "December 15, 2024 @ 1:00PM EST",
+        eventName: "Sed suscipit elementum interdum",
+        description: "Vestibulum egestas imperdiet sem, dignissim suscipit felis fringilla in. Aenean id erat at ante vulputate varius.",
+        entranceFee: 10.00,
+        eventID: 5
+    }
+]
