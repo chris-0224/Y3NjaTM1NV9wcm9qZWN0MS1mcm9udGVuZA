6cb99110cac303497bd2057f917d8919bf9b4301
chris-0224 <chrisperez2589@gmail.com>
2024-03-10 23:59:43 -0400

    Minor change to number of items produced

--- src/data-interface.js
+++ src/data-interface.js
@@ -2,10 +2,10 @@
 export function getStoreItems(){
     const items = [];
 
-    for (let i= 0; i<10; i++){
+    for (let i= 0; i < 8; i++){
         items.push(
             {
-                name: `Item ${i}`,
+                name: `Item ${i+1}`,
                 imageUrl: null,
                 price: Math.floor(Math.random() * 100) + 0.99,
                 notes: "Limited Edition!"
