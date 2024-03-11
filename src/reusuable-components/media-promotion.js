80553e385f2a6a7a383441944a5f61c72f04e612
chris-0224 <chrisperez2589@gmail.com>
2024-03-11 00:05:58 -0400

    - Added height prop

--- src/reusuable-components/media-promotion.js
+++ src/reusuable-components/media-promotion.js
@@ -1,12 +1,12 @@
 import placeholder_img from "../media/placeholder-image.png";
 
-export default function MediaPromotion({title, img, desc, width, attribute, id}){
+export default function MediaPromotion({title, img, desc, width, height, attribute, id}){
     const image = img ? img : placeholder_img;
 
     return (
         <div className="media-promo-container" id={id}>
             <div className="image-container">
-                <img src={image} alt="promotion" width={width}/>
+                <img src={image} alt="promotion" width={width} height={height}/>
                 <cite>{attribute}</cite>
             </div>
             <div className="content-body-container">
